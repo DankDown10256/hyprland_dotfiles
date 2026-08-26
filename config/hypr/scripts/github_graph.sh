ff9560c440221600b3e6fb86b39a418e39c921e9
Lucas Poupeau <lucasp.linux@gmail.com>
2026-08-26 22:23:46 +0200

    ui: huge refont and change the config to a more lighweight one and developer friendly removes matugen and others unecessary configs

--- config/hypr/scripts/github_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remplace 'ton_pseudo' par ton vrai nom d'utilisateur GitHub
-USERNAME="DankDown10256"
-OUTPUT_PATH="$HOME/.config/hypr/github_contributions.png"
-
-# Télécharge le graphe en mode sombre (mieux pour hyprlock)
-# On ajoute un paramètre de temps pour éviter que le cache ne nous montre une vieille image
-curl -s "https://ghchart.rshah.org/40c463/${USERNAME}?$(date +%s)" -o "$OUTPUT_PATH"
